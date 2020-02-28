2cf734bc81d1927d0116d717eac46f321b767a6d
ramasd <ramunas.danupas@gmail.com>
2020-02-28 06:00:53 +0200

    Added possibility to delete a customer.

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -37,13 +37,13 @@
                                 <td>Name</td>
                                 <td>Email</td>
                                 <td colspan = 2>Actions</td>
-                                <td>Checkbox</td>
+                                <td><input type="checkbox" class="checkbox_all"></td>
                             </tr>
                         </thead>
 
                         <tbody>
                             @foreach($customers as $customer)
-                                <tr>
+                                <tr id="customer-{{ $customer->id }}">
                                     <td>{{ $customer->id }}</td>
                                     <td>{{ $customer->first_name }} {{ $customer->last_name }}</td>
                                     <td>{{ $customer->email }}</td>
@@ -51,14 +51,9 @@
                                         <a href="{{ route('customers.edit',$customer->id) }}" class="btn btn-primary">Edit</a>
                                     </td>
                                     <td>
-                                        {{-- <form action="{{ route('customers.destroy', $customer->id) }}" method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-danger" type="submit">Delete</button>
-                                        </form> --}}
+                                        <button class="deleteRecord btn btn-danger delete_user" data-token="{{ csrf_token() }}" id="{{ $customer->id }}">Delete</button>
                                     </td>
-                                    <td><input type="checkbox" name="selected_customers[]" value="{{ $customer->id }}"></td>
-                                {{-- <td><input type="checkbox" name="selected_customers[]" id="checkbox-{{ $customer->id }}" onclick="alert()" value="{{ $customer->id }}"></td> --}}
+                                    <td><input type="checkbox" class="selected_customers" name="selected_customers[]" value="{{ $customer->id }}"></td>
                                 </tr>
                             @endforeach
                         </tbody>
@@ -71,4 +66,38 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+
+<script>
+    $(document).ready(function(){
+        $(".checkbox_all").click(function(){
+            $('input.selected_customers').prop('checked', this.checked);
+        });
+
+        $(".deleteRecord").click(function(){
+            if(confirm('Are you sure?') ){
+                var id = $(this).attr("id");
+                var token = $("meta[name='csrf-token']").attr("content");
+    
+                $.ajax(
+                {
+                    url: "customers/"+id,
+                    type: 'post',
+                    data: {
+                        _method: 'delete',
+                        _token :token
+                    },
+                    success: function (){
+                        $('#customer-' + id).remove();
+                    },
+                    error: function(xhr) {
+                        console.log(xhr.responseText);
+                    }
+                });
+            }
+        });
+    });
+</script>
 @endsection
